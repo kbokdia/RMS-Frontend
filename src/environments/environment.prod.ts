be77935d27d41d86068bd2199f8832855ed73d44
Kamlesh Bokdia <kbokdia@gmail.com>
2022-06-12 02:55:20 +0530

    Updated env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseApiUrl: 'https://rms-appservice.azurewebsites.net/api',
+  baseApiUrl: 'api',
   security: {
     allowedOrigins: 'https://rms-appservice.azurewebsites.net/api/Table'
   }
